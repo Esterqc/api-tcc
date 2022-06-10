633dd59136153f1b1e243d3a9be4516f3f73ba85
ester queiroz de carvalho <esterq292@gmail.com>
2022-06-10 00:10:50 -0300

    oii

--- api/src/controller/agendamentoController.js
+++ api/src/controller/agendamentoController.js
@@ -10,6 +10,32 @@ server.post('/agendar',async (req,resp) =>{
         if(!novoAgendarmento.nome)
         throw new Error('Nome Do Paciente é obrigatorio!')
 
+        if(!novoAgendarmento.idade)
+        throw new Error('idade Do Paciente é obrigatorio!')
+
+        if(!novoAgendarmento.nasc)
+        throw new Error('Data de nasc Do Paciente é obrigatorio!')
+
+        if(!novoAgendarmento.cpf)
+        throw new Error('cpf Do Paciente é obrigatorio!')
+
+        if(!novoAgendarmento.rg)
+        throw new Error('rg Do Paciente é obrigatorio!')
+
+        if(!novoAgendarmento.doutor)
+        throw new Error(' nome do doutor é obrigatorio!')
+
+        if(!novoAgendarmento.servico)
+        throw new Error(' servico é obrigatorio!')  
+
+        if(!novoAgendarmento.  agendamento)
+        throw new Error('  agendamento é obrigatorio!')
+
+
+
+
+        
+
         const Agendarconsulta = await InserirAgendamento(novoAgendarmento);
 
         resp.send(Agendarconsulta);
